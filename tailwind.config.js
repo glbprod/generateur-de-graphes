d2a3a95810206311d9a6677cda8d7ab1fd706b51
GLB <108488004+glbprod@users.noreply.github.com>
2025-10-30 01:50:20 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class', // 👈 indispensable pour le bouton toggle
+  darkMode: 'class', // 👈 nécessaire pour le bouton "mode sombre"
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // palette douce pour dark mode
+        darkbg: '#111827',
+        darkcard: '#1f2937',
+        darktext: '#e5e7eb',
+      },
+    },
   },
   plugins: [],
 }
